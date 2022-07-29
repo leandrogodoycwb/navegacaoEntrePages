48176e3411d7008b583d70f6d241385cd4e406f3
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2022-07-28 22:30:31 -0300

    valueNotifier

--- lib/one_page.dart
+++ lib/one_page.dart
@@ -1,18 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:navigation_page/widgets/custom_button_widget.dart';
 
-class OnePage extends StatelessWidget {
+class OnePage extends StatefulWidget {
   const OnePage({Key? key}) : super(key: key);
 
   @override
+  _OnePageState createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+  ValueNotifier<int> valorAleatorio = ValueNotifier<int>(0);
+
+  random() async {
+    for (int i = 0; i < 10; i++) {
+      await Future.delayed(Duration(seconds: 2));
+
+      valorAleatorio.value = Random().nextInt(1000);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Scaffold(
       body: Center(
-        child: CustomButtonWidget(
-          disable: false,
-          onPressed: () {},
-          title: 'Custom BTN',
-          titleSize: 20,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: valorAleatorio,
+              builder: (_, value, __) => Text(
+                'Valor eh: $value',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 10),
+            CustomButtonWidget(
+              disable: false,
+              onPressed: () => random(),
+              title: 'Custom BTN',
+              titleSize: 20,
+            ),
+          ],
         ),
       ),
     );
